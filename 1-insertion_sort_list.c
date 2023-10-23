48c535d3c2ad3a657701ae5d4c0cfb3e87de0ad4
tarie100 <tarie.school@gmail.com>
2023-10-23 12:38:10 -0700

    1-insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include"sort.h"
+/**
+ * swap_node- Swaps two nodes
+ * @h: pointer to head
+ * @n1: pointer to 1st node
+ * @n2: pointer to 2nd node
+ */
+void swap_node(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+/**
+ * insertion_sort_list -  sorts a doubly linked list of integers in ascending
+ * @list: the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i, *insert, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (i = (*list)->next; i != NULL; i = temp)
+	{
+		temp = i->next;
+		insert = i->prev;
+		while (insert != NULL && i->n < insert->n)
+		{
+			swap_node(list, &insert, i);
+			print_list((const listint_t *)*list);
+		}
+	}
+}
